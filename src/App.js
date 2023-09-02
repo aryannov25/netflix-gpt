3ca59f05f4571b87a21db8bc8418b3b2baa61c3b
Aryan Chaudhary <aryannov25@gmail.com>
2023-09-02 23:18:26 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className="text-center">
+      <header className="font-bold">
+      
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
