cbdf421549a4b7d5926069cc9bb2ceab2754db13
Aryan Chaudhary <aryannov25@gmail.com>
2023-09-07 21:02:43 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,18 +1,9 @@
-<<<<<<< HEAD
 import "./App.css";
-=======
-import './App.css';
->>>>>>> 3ca59f05f4571b87a21db8bc8418b3b2baa61c3b
 
 function App() {
   return (
     <div className="text-center">
-<<<<<<< HEAD
       <header className="font-extrabold">
-=======
-      <header className="font-bold">
-      
->>>>>>> 3ca59f05f4571b87a21db8bc8418b3b2baa61c3b
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
